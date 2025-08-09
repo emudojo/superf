f7b74de4742d5311fbf4e35c50a7ad05d4780f5d
Hertzel Armengol Rivera <emudojo@gmail.com>
2025-08-09 16:28:20 -0600

    cambios

--- test_menu.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Testing SuperF Main Menu Display
-echo ================================
-cd /d "C:\Users\Cyber Power\OneDrive\Fidelitas\Calidad de Software\com.mycompany_SuperF_jar_1.0-SNAPSHOT"
-java -cp target\classes com.mycompany.superf.SuperF
-pause
